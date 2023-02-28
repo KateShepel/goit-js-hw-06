e5689c79a6c50756785590bc1f07b784eb38260c
KateShepel <MariyaKaterina@gmail.com>
2023-02-28 15:27:17 +0200

    Додає ДЗ№2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const itemsElArray = [];
+
+for (const ingredient of ingredients) {
+  const itemsEl = document.createElement('li');
+  itemsEl.classList.add('item');
+  itemsEl.textContent = ingredient
+  itemsElArray.push(itemsEl);
+}
+
+// console.log(itemsElArray);
+
+
+const listEl = document.querySelector('#ingredients');
+listEl.append(...itemsElArray);
+
+// console.log(listEl)
