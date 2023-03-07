17081cd6623a5899e904736e6db77a3ed5b3eb48
KateShepel <MariyaKaterina@gmail.com>
2023-03-07 16:37:42 +0200

    Додає ДЗ№8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (mail === "" || password === "") {
+        alert("Fill in all the fields");
+        return;
+    }
+
+    const result = {
+            mail,
+            password,
+        };
+
+        console.log(result);
+        form.reset();
+
+}
