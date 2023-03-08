dfce7778f8f6b3093b321c4dd87f148de7a653c2
KateShepel <MariyaKaterina@gmail.com>
2023-03-08 10:19:10 +0200

    Додає ДЗ№9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.querySelector('body'),
+  btn: document.querySelector('.change-color'),
+  span: document.querySelector('.color'),
+}
+
+refs.btn.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  const newColor = getRandomHexColor();
+  refs.body.style.backgroundColor = newColor;
+  refs.span.textContent = newColor;
+}
